b380cd7ec5dae9464aa2fb853e798aef4ca7ea5f
orik <artikofman@gmail.com>
2020-01-15 21:24:07 +0200

    JQuery starts

---
+++ JQueryTask1/index.js
@@ -0,0 +1,42 @@
+// console.log('Script Ready',new Date().getTime());
+
+// $(document).ready(function () {
+//     // console.log('Dom Ready',new Date().getTime());
+
+//     $('button').click(someFunction);
+//     document.getElementsByTagName('button')[0]
+//         .addEventListener("click", (e) => {
+//             console.log('addEventListener', e);
+
+//         })
+// });
+
+// function someFunction(e) {
+//     $('.h').toggle(4000).toggle(200);
+//     console.log('someFunction', e);
+// }
+
+// function someFunction1() {
+//     $('body')
+//         .append("<h1>😎someFunction!!</h1>")
+
+// }
+
+// $(document).ready(setEventListners);
+
+// function setEventListners() {
+//     $("#btn1").click(() => $("#mark").css("background", "yellow"));
+// }
+
+// $("p.p1").css("background", "darkblue")
+
+//$("#mark2 li").css("background", "darkblue")
+// $("ul li").css("background", "darkblue")
+
+$("ul li.mark3").css("background", "darkblue");
+$('a[target="_blank"]').css("background", "yellow");
+$("td:odd").css("background", "darkblue");
+$("td:even").css("background", "brown");
+$("form :password").css("background", "purple");
+$("button:hover").css("background-color", "rgb(42, 165, 89)");
+$("button:hover").css("transition", "all 1s ease-in-out");
